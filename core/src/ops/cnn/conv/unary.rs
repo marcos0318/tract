f513155a86e067e2aa88dd6c4c3572630b718435
Mathieu Poumeyrol <kali@zoy.org>
2020-10-27 14:40:32 +0100

    fused is now dynamically types

--- core/src/ops/cnn/conv/unary.rs
+++ core/src/ops/cnn/conv/unary.rs
@@ -105,7 +105,7 @@ impl ConvUnary {
         Ok(packed_as.insert_axis(Axis(0)))
     }
 
-    fn bias_as_non_linear<T>(&self) -> TractResult<Option<ArrayD<Vec<FusedSpec<T>>>>>
+    fn bias_as_non_linear<T>(&self) -> TractResult<Option<ArrayD<Vec<FusedSpec>>>>
     where
         T: Datum + Copy,
     {
@@ -119,7 +119,7 @@ impl ConvUnary {
                     bias.iter()
                         .chunks(self.output_channels() / self.group)
                         .into_iter()
-                        .map(|c| vec![FusedSpec::PerRowAdd(c.into_iter().cloned().collect())])
+                        .map(|c| vec![FusedSpec::PerRowAdd(tensor1(&*c.cloned().collect::<Vec<_>>()))])
                         .collect(),
                 )?
                 .into_dyn(),
@@ -265,7 +265,7 @@ impl ConvUnary {
                 c_fact: TypedFact::dt_shape(TC::datum_type(), &*output_shape.shape)?,
                 c_prefix_dim_and_stride,
                 packed_as: self.kernel_as_packed_as::<TA>(&mmm.a_pack())?,
-                fused_ops: self.bias_as_non_linear()?,
+                fused_ops: self.bias_as_non_linear::<TI>()?,
                 mmm,
             },
             &[wire],
